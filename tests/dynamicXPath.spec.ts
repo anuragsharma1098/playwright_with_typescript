eb184c19dd7306d8962fc3fb5d2e9fd19359dae3
anuragsharma1098 <anuragsharma1098@gmail.com>
2025-12-19 02:00:35 +0530

    XPaths

--- tests/dynamicXPath.spec.ts
+++ tests/dynamicXPath.spec.ts
@@ -4,13 +4,13 @@ test('Dynamic WebElement Handling', async ({ page }) => {
 
     page.goto("https://testautomationpractice.blogspot.com/");
     for (let i = 1; i <= 5; i++) {
-        let button: Locator = page.locator('//button[text()="STOP" or text()="START"]');
-        // let button = await page.locator('//button[@name="start"]');
-        // let button = await page.locator('//button[@name="start" or @name="stop"]');
-        // let button = await page.locator('//button[contains(@name,"st")]');
-        // let button = await page.locator('//button[starts-with(@name,"st")]');
+        // let button: Locator = page.locator('//button[text()="STOP" or text()="START"]');
+        // // let button = await page.locator('//button[@name="start"]');
+        // // let button = await page.locator('//button[@name="start" or @name="stop"]');
+        // // let button = await page.locator('//button[contains(@name,"st")]');
+        // // let button = await page.locator('//button[starts-with(@name,"st")]');
+        const button = page.getByRole('button', { name: /START|STOP/ })
         await button.click();
         await page.waitForTimeout(2000);
-
     }
 });
