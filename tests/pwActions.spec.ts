416a23662baafc3104bf12b727c4ae2d862e9a58
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-01-12 19:49:42 +0530

    fixed locator issue

--- tests/pwActions.spec.ts
+++ tests/pwActions.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, Locator } from '@playwright/test';
 
 test('Text Input Action', async ({ page }) => {
     await page.goto('https://testautomationpractice.blogspot.com/');
-    const textBox: Locator = page.locator('name');
+    const textBox: Locator = page.locator('input[placeholder="Enter Name"]');
     await expect(textBox).toBeVisible();
     await expect(textBox).toBeEnabled();
     await textBox.fill('Name Name');
