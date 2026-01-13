a5c49ef1b8d8fc1a9bc288984ed23b7bc75ac245
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-01-13 23:31:02 +0530

    Checkboxes radio btns

--- tests/pwActions.spec.ts
+++ tests/pwActions.spec.ts
@@ -5,9 +5,55 @@ test('Text Input Action', async ({ page }) => {
     const textBox: Locator = page.locator('input[placeholder="Enter Name"]');
     await expect(textBox).toBeVisible();
     await expect(textBox).toBeEnabled();
+    const maxLength: string | null = await textBox.getAttribute('maxlength');
+    expect(maxLength).toBe('15');
     await textBox.fill('Name Name');
+    console.log('Input value of the text box: ',await textBox.inputValue());
+    expect(await textBox.inputValue()).toBe('Name Name');
+    await page.waitForTimeout(2000);
 
+    const maleRadio: Locator = page.locator("//input[@id='male']");
+    await expect(maleRadio).toBeVisible();
+    await expect(maleRadio).toBeEnabled();
+    await maleRadio.check();
+    expect(await maleRadio.isChecked()).toBeTruthy();
+    await page.waitForTimeout(2000);
 
+    const sunday = page.getByLabel('Sunday');
+    await expect(sunday).toBeVisible();
+    await expect(sunday).toBeEnabled();
+    await sunday.check();
+    expect(await sunday.isChecked()).toBeTruthy();
+    await page.waitForTimeout(2000);
 
+    const days:string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const checkboxes: Locator[] = days.map(index => page.getByLabel(index));
+    expect(checkboxes.length).toBe(6);
+    // for (const day of days) {
+    //     const dayLocator = page.getByLabel(day);
+    //     await expect(dayLocator).toBeVisible();
+    //     await expect(dayLocator).toBeEnabled();
+    //     await dayLocator.check();
+    //     expect(await dayLocator.isChecked()).toBeTruthy();
+    //     await page.waitForTimeout(1000);
+    // }
+    for (const checkbox of checkboxes) {
+        await checkbox.check();
+        await expect(checkbox).toBeChecked();
+    }
 
+    // uncheck monday tuesday friday
+    const uncheckDays:string[] = ['Monday', 'Tuesday', 'Friday'];
+    // for (const day of uncheckDays) {
+    //     const dayLocator = page.getByLabel(day);
+    //     await expect(dayLocator).toBeVisible();
+    //     await expect(dayLocator).toBeEnabled();
+    //     await dayLocator.uncheck();
+    //     expect(await dayLocator.isChecked()).toBeFalsy();
+    //     await page.waitForTimeout(1000);
+    // }
+    for (const day of uncheckDays) {
+        await checkboxes[days.indexOf(day)].uncheck();
+        await expect(checkboxes[days.indexOf(day)]).not.toBeChecked();
+    }
 });
