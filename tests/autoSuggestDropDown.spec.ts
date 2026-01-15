b53aacda1a7cda84eb14dd043ee4d218cc237542
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-01-16 03:40:42 +0530

    .

---
+++ tests/autoSuggestDropDown.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect, Locator } from '@playwright/test';
+
+test('Auto-suggest drop-down functionality', async ({ page }) => {
+    await page.goto('https://www.flipkart.com/');
+
+    await page.locator('input[name="q"]').fill('smart');
+    await page.waitForTimeout(8000); // wait for suggestions to load
+    
+    // get all the suggested options -->ctrl+shipt+p on DOM --> emulate focused page
+    const suggestions: Locator = page.locator("ul>li");
+    const suggestionCount = await suggestions.count();
+
+    console.log('Total suggestions: ' + suggestionCount);
+
+    // printing all the suggested options
+    console.log('Suggested options:');
+    for (let i = 0; i < suggestionCount; i++) {
+        const suggestionText = await suggestions.nth(i).textContent();
+        console.log(suggestionText);
+    }
+
+
+
+
+});
