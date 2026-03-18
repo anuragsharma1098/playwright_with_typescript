abd7e5b5b8df5d1fd8e26096a803e36f7605faff
anuragsharma1098 <anuragsharma1098@gmail.com>
2026-03-18 16:13:09 +0530

    test ci

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -17,7 +17,7 @@ import { test, expect } from '@playwright/test';
  */
 test('Google Homepage', async ({ page }) => {
   //Navigate to Google Homepage
-  await page.goto('https://www.google.com');
+  await page.goto('https://www.google.com/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Google/);
